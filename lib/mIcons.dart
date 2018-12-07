82979f865f11d72671742b9067a2d098f0cdeb03
Nobuhito SATO <nobuhito.sato@gmail.com>
2018-12-07 21:57:50 +0900

    Supports category display

--- lib/mIcons.dart
+++ lib/mIcons.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -5,15 +6,24 @@ class MIcon {
   String key;
   int codepoint;
   IconData iconData;
+  String category;
 
   MIcon(
-      {@required this.key, @required this.codepoint, @required this.iconData});
+      {@required this.key,
+      @required this.codepoint,
+      @required this.iconData,
+      this.category = ""});
 }
 
 class MIcons {
   List<MIcon> items = [];
+  List<String> categories = [];
 
-  Future<List<MIcon>> fetchIcons() async {
+  Map<String, int> _codepoints = {};
+
+  MIcons();
+
+  Future<Null> fetchCodepoints() async {
     String url =
         "https://raw.githubusercontent.com/flutter/flutter/master/packages/flutter/lib/src/material/icons.dart";
 
@@ -25,17 +35,41 @@ class MIcons {
     http.Response res = await http.get(url);
     regexp.allMatches(res.body).forEach((match) {
       String key = match.group(1);
-      int codepoint = int.tryParse(match.group(2), radix: 16);
-      IconData iconData = IconData(codepoint, fontFamily: "MaterialIcons");
-      this.items.add(
-            MIcon(
-              key: key,
-              codepoint: codepoint,
-              iconData: iconData,
-            ),
+      String codepoint = match.group(2);
+
+      this._codepoints[key] = int.tryParse(codepoint, radix: 16);
+    });
+  }
+
+  Future<MIcons> fetch() async {
+    if (this._codepoints.length == 0) {
+      await fetchCodepoints();
+    }
+
+    String url = "https://material.io/tools/icons/static/data.json";
+    http.Response res = await http.get(url);
+
+    var json = jsonDecode(res.body);
+    json["categories"].forEach((category) {
+      category["icons"].forEach((_icon) {
+        String key = _icon["id"].toString();
+        if (this._codepoints.containsKey(key)) {
+          MIcon icon = MIcon(
+            key: key,
+            codepoint: this._codepoints[key],
+            iconData:
+                IconData(this._codepoints[key], fontFamily: "MaterialIcons"),
+            category: category["name"],
           );
+          this.items.add(icon);
+
+          if (!this.categories.contains(category["name"])) {
+            this.categories.add(category["name"]);
+          }
+        }
+      });
     });
 
-    return this.items;
+    return this;
   }
 }
